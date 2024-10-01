04a02d3d43e30c025a71eb9ed9d5cee9078f7a0d
Franco <francocorniglione5@gmail.com>
2024-10-01 19:58:18 -0300

    uploading AutoDeportivo class

---
+++ pilares-poo/src/AutoDeportivo.java
@@ -0,0 +1,28 @@
+public class AutoDeportivo extends Automovil {
+    private boolean esConvertible;
+
+    public AutoDeportivo() {
+    }
+
+    public AutoDeportivo(String marca) {
+        super(marca);
+    }
+
+    public AutoDeportivo(String marca, String modelo, int anioFabricacion, boolean esConvertible) {
+        super(marca, modelo, anioFabricacion);
+        this.esConvertible = esConvertible;
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println("Acelera muy rápido");
+    }
+
+    public boolean isEsConvertible() {
+        return esConvertible;
+    }
+
+    public void setEsConvertible(boolean esConvertible) {
+        this.esConvertible = esConvertible;
+    }
+}
