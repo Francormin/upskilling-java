7db24fafcdfd4ee3a0b4bff63280ccc479e6eac5
Franco <francocorniglione5@gmail.com>
2024-10-02 19:26:28 -0300

    Main entry point file

---
+++ clase-String/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // EjemploLength.ejecutarLength();
+        // EjemploCharAt.ejecutarCharAt();
+        // EjemploSubstring.ejecutarSubstring();
+        // EjemploReplace.ejecutarReplace();
+        // EjemploConcat.ejecutarConcat();
+        // EjemploEqualsIgnoreCase.ejecutarEqualsIgnoreCase();
+        // EjemploToLowerToUpperCase.ejecutarToLowerToUpperCase();
+        // EjemploTrim.ejecutarTrim();
+    }
+}
