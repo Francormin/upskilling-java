fb248f7f6f90f1204a4530c5838b7f382ae18b18
Franco <francocorniglione5@gmail.com>
2024-10-02 19:23:30 -0300

    substring method

---
+++ clase-String/src/EjemploSubstring.java
@@ -0,0 +1,7 @@
+public class EjemploSubstring {
+    public static void ejecutarSubstring() {
+        String texto = "Hola Mundo Java";
+        String subcadena = texto.substring(5, 10);
+        System.out.println("El nuevo String es: " + subcadena);
+    }
+}
