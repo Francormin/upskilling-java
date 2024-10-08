88e1d107ea595d7e4a44308b0d6987cc52403f75
Franco <francocorniglione5@gmail.com>
2024-10-08 20:46:16 -0300

    Rey child class

---
+++ homework-1/src/piezas/Rey.java
@@ -0,0 +1,10 @@
+package piezas;
+
+public class Rey extends PiezaAjedrez {
+    public Rey() {
+        super(
+                "Rey",
+                "Una casilla en cualquier dirección."
+        );
+    }
+}
