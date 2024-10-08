e73a6b3bb20bbb74d1cc3c475a137a5da0bced7a
Franco <francocorniglione5@gmail.com>
2024-10-08 20:45:41 -0300

    Caballo child class

---
+++ homework-1/src/piezas/Caballo.java
@@ -0,0 +1,10 @@
+package piezas;
+
+public class Caballo extends PiezaAjedrez {
+    public Caballo() {
+        super(
+                "Caballo",
+                "En forma de 'L', pudiendo saltar sobre otras piezas en su recorrido."
+        );
+    }
+}
