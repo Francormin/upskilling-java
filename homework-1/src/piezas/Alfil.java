0dce81e6d0468d29b07b2a97443b3d7fbf90155f
Franco <francocorniglione5@gmail.com>
2024-10-08 20:45:55 -0300

    Alfil child class

---
+++ homework-1/src/piezas/Alfil.java
@@ -0,0 +1,10 @@
+package piezas;
+
+public class Alfil extends PiezaAjedrez {
+    public Alfil() {
+        super(
+                "Alfil",
+                "Diagonalmente las casillas que quiera."
+        );
+    }
+}
