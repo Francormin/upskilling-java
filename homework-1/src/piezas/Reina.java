f7178f09ec22c13d7113de9c6937e33b252a56a2
Franco <francocorniglione5@gmail.com>
2024-10-08 20:46:05 -0300

    Reina child class

---
+++ homework-1/src/piezas/Reina.java
@@ -0,0 +1,10 @@
+package piezas;
+
+public class Reina extends PiezaAjedrez {
+    public Reina() {
+        super(
+                "Reina",
+                "En cualquier dirección las casillas que quiera."
+        );
+    }
+}
