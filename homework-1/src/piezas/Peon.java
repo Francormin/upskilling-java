a23f5beddeb4258642ab41676e32b2dd306eecf0
Franco <francocorniglione5@gmail.com>
2024-10-08 20:45:11 -0300

    Peon child class

---
+++ homework-1/src/piezas/Peon.java
@@ -0,0 +1,10 @@
+package piezas;
+
+public class Peon extends PiezaAjedrez {
+    public Peon() {
+        super(
+                "Peón",
+                "Una casilla hacia adelante, pudiéndose mover dos casillas en su primer movimiento y captura en diagonal."
+        );
+    }
+}
