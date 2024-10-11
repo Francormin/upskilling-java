e163ba05a2ad8f413744958aa9d941d7bfb049cc
Franco <francocorniglione5@gmail.com>
2024-10-11 10:10:18 -0300

    Persona class

---
+++ homework-4/src/Persona.java
@@ -0,0 +1,48 @@
+public class Persona {
+    private String nombre;
+    private Integer edad;
+    private String hobby;
+
+    public Persona() {
+
+    }
+
+    public Persona(String nombre, Integer edad, String hobby) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.hobby = hobby;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
