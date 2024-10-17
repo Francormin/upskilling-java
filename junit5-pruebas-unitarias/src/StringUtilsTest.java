f087ff3035edb9ffc60988c9f3dca16714c8974a
Franco <francocorniglione5@gmail.com>
2024-10-17 11:13:07 -0300

    displayName small change

--- junit5-pruebas-unitarias/src/StringUtilsTest.java
+++ junit5-pruebas-unitarias/src/StringUtilsTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Pruebas para StringUtils")
+@DisplayName("Pruebas unitarias para StringUtils")
 class StringUtilsTest {
 
     @Test
