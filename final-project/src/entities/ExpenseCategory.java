9c7d9607d3ddfcdef6adf83a93d864d7f4276d85
Franco <francocorniglione5@gmail.com>
2024-10-26 11:30:48 -0300

    ExpenseCategory model stuff

---
+++ final-project/src/entities/ExpenseCategory.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class ExpenseCategory {
+    private static Integer counter = 0;
+    private Integer id;
+    private String name;
+    private String description;
+
+    public ExpenseCategory() {
+    }
+
+    public ExpenseCategory(String name) {
+        this.id = ++counter;
+        this.name = name.toLowerCase();
+    }
+
+    public ExpenseCategory(String name, String description) {
+        this(name);
+        this.description = description.toLowerCase();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.toLowerCase();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return "{ " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                " }";
+    }
+}
