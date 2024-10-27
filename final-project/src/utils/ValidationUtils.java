6f01735266fc2fb310b6664bc25655cd9104ff2a
Franco <francocorniglione5@gmail.com>
2024-10-27 18:37:10 -0300

    Validation stuff

---
+++ final-project/src/utils/ValidationUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class ValidationUtils {
+    public static boolean isBlank(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    public static boolean isValidExpenseCategoryName(String expenseCategoryName) {
+        return expenseCategoryName.matches("[a-zA-Z]+");
+    }
+
+    public static int validateInteger(String input) throws NumberFormatException {
+        return Integer.parseInt(input);
+    }
+}
