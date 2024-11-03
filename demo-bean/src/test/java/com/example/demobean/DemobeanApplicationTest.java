57c1d68f8137d1dec4f645223aa133e1568c269f
Franco <francocorniglione5@gmail.com>
2024-11-03 12:52:05 -0300

    test folder stuff

---
+++ demo-bean/src/test/java/com/example/demobean/DemobeanApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.demobean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobeanApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
