8ad525f2c84b6bc267ad559c0a45f779d82b8f5c
Franco <francocorniglione5@gmail.com>
2024-11-05 11:20:35 -0300

    model folder stuff

---
+++ demo-rest/src/main/java/com/example/demorest/model/Movie.java
@@ -0,0 +1,42 @@
+package com.example.demorest.model;
+
+public class Movie {
+
+    private Integer id;
+    private String title;
+    private String director;
+    private Integer releaseYear;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+}
