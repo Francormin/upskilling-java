ed491d8842404ba8044b18c691b7bf3b3dba7df3
Franco <francocorniglione5@gmail.com>
2024-11-06 10:00:44 -0300

    model folder stuff

---
+++ homework-rest/src/main/java/com/example/homeworkrest/model/Song.java
@@ -0,0 +1,42 @@
+package com.example.homeworkrest.model;
+
+public class Song {
+
+    private int id;
+    private String title;
+    private String artist;
+    private int releaseYear;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+}
