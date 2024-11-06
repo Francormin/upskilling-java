24cf45fff28486d06953dbfef89bb456db427e92
Franco <francocorniglione5@gmail.com>
2024-11-06 10:01:26 -0300

    entry point file

---
+++ homework-rest/src/main/java/com/example/homeworkrest/HomeworkrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkrestApplication.class, args);
+	}
+
+}
