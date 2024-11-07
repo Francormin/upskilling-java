4a8c54bd7d96a4726238b70dcf931682e72fb561
Franco <francocorniglione5@gmail.com>
2024-11-07 10:28:28 -0300

    model folder stuff

---
+++ demo-mvc/src/main/java/com/example/demomvc/model/Transaction.java
@@ -0,0 +1,49 @@
+package com.example.demomvc.model;
+
+public class Transaction {
+
+    private Long id;
+    private Double amount;
+    private String description;
+    private String date;
+
+    public Transaction(Long id, Double amount, String description, String date) {
+        this.id = id;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
