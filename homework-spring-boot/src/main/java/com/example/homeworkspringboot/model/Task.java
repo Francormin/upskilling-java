b8d059cfa6d75b71bd9b1d13e6370c5d2b21862b
Franco <francocorniglione5@gmail.com>
2024-11-09 17:26:23 -0300

    updating Task model

--- homework-spring-boot/src/main/java/com/example/homeworkspringboot/model/Task.java
+++ homework-spring-boot/src/main/java/com/example/homeworkspringboot/model/Task.java
@@ -1,17 +1,9 @@
 package com.example.homeworkspringboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 import java.util.Date;
 
-@Entity
 public class Task {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String description;
