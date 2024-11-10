2eb213af37a23104c33b0389fac82c2d1147228a
Franco <francocorniglione5@gmail.com>
2024-11-10 17:57:20 -0300

    model folder stuff

---
+++ demo-jdbc-template/src/main/java/com/example/demojdbctemplate/model/Ticket.java
@@ -0,0 +1,33 @@
+package com.example.demojdbctemplate.model;
+
+public class Ticket {
+
+    private Long id;
+    private String description;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
