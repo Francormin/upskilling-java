a1e4df9fea02ef044fd2630748c10a8774b89b96
Franco <francocorniglione5@gmail.com>
2024-11-10 17:58:18 -0300

    entry point file

---
+++ demo-jdbc-template/src/main/java/com/example/demojdbctemplate/DemojdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojdbctemplateApplication.class, args);
+	}
+
+}
