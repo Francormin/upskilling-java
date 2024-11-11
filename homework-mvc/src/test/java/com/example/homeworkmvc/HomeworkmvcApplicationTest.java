2cb7c4b5000cddb500e55aa9101422ad820d9fa9
Franco <francocorniglione5@gmail.com>
2024-11-11 17:08:33 -0300

    test folder stuff

---
+++ homework-mvc/src/test/java/com/example/homeworkmvc/HomeworkmvcApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.homeworkmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkmvcApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
