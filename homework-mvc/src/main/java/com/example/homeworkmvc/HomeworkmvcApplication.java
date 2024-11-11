c3c4d9e1e7c2ab253d614dff75f80dd643e2b0df
Franco <francocorniglione5@gmail.com>
2024-11-11 17:12:02 -0300

    entry point file

---
+++ homework-mvc/src/main/java/com/example/homeworkmvc/HomeworkmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkmvcApplication.class, args);
+	}
+
+}
