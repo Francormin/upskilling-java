ada874dc74eef8e6ef72434342232601636511d8
Franco <francocorniglione5@gmail.com>
2024-11-12 11:57:45 -0300

    sql statements refactoring stuff

--- homework-jdbc-template/src/main/java/com/example/homeworkjdbctemplate/repository/impl/JdbcMovieRepository.java
+++ homework-jdbc-template/src/main/java/com/example/homeworkjdbctemplate/repository/impl/JdbcMovieRepository.java
@@ -1,5 +1,6 @@
 package com.example.homeworkjdbctemplate.repository.impl;
 
+import com.example.homeworkjdbctemplate.constants.MovieSqlQueries;
 import com.example.homeworkjdbctemplate.model.Movie;
 import com.example.homeworkjdbctemplate.repository.MovieRepository;
 
@@ -15,49 +16,59 @@ public class JdbcMovieRepository implements MovieRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    private static final String GET_ALL_MOVIES = "SELECT * FROM movies";
-    private static final String GET_MOVIE_BY_ID = "SELECT * FROM movies WHERE id = ?";
-    private static final String GET_MOVIE_BY_TITLE = "SELECT * FROM movies WHERE title LIKE '%' || ? || '%'";
-    private static final String CREATE_MOVIE = "INSERT INTO movies (title, director, release_year) VALUES (?, ?, ?)";
-    private static final String UPDATE_MOVIE = "UPDATE movies SET title = ?, director = ?, release_year = ? WHERE id = ?";
-    private static final String DELETE_MOVIE = "DELETE FROM movies WHERE id = ?";
-
     public JdbcMovieRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public List<Movie> findAll() {
-        return jdbcTemplate.query(GET_ALL_MOVIES, new BeanPropertyRowMapper<>(Movie.class));
+        return jdbcTemplate.query(MovieSqlQueries.GET_ALL, new BeanPropertyRowMapper<>(Movie.class));
     }
 
     @Override
     public Optional<Movie> findById(Long id) {
-        return jdbcTemplate.query(GET_MOVIE_BY_ID, new BeanPropertyRowMapper<>(Movie.class), id)
+        return jdbcTemplate.query(
+                MovieSqlQueries.GET_BY_ID,
+                new BeanPropertyRowMapper<>(Movie.class),
+                id)
             .stream()
             .findFirst();
     }
 
     @Override
     public Optional<Movie> findByTitle(String title) {
-        return jdbcTemplate.query(GET_MOVIE_BY_TITLE, new BeanPropertyRowMapper<>(Movie.class), title)
+        return jdbcTemplate.query(
+                MovieSqlQueries.GET_BY_TITLE,
+                new BeanPropertyRowMapper<>(Movie.class),
+                title)
             .stream()
             .findFirst();
     }
 
     @Override
     public int save(Movie movie) {
-        return jdbcTemplate.update(CREATE_MOVIE, movie.getTitle(), movie.getDirector(), movie.getReleaseYear());
+        return jdbcTemplate.update(
+            MovieSqlQueries.CREATE,
+            movie.getTitle(),
+            movie.getDirector(),
+            movie.getReleaseYear()
+        );
     }
 
     @Override
     public int update(Long id, Movie movie) {
-        return jdbcTemplate.update(UPDATE_MOVIE, movie.getTitle(), movie.getDirector(), movie.getReleaseYear(), id);
+        return jdbcTemplate.update(
+            MovieSqlQueries.UPDATE,
+            movie.getTitle(),
+            movie.getDirector(),
+            movie.getReleaseYear(),
+            id
+        );
     }
 
     @Override
     public int deleteById(Long id) {
-        return jdbcTemplate.update(DELETE_MOVIE, id);
+        return jdbcTemplate.update(MovieSqlQueries.DELETE, id);
     }
 
 }
