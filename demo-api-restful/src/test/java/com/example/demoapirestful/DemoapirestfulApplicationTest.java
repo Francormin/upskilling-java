44fae2798d6d6bc50bf54ba1be2093b27df5f363
Franco <francocorniglione5@gmail.com>
2024-11-14 10:19:47 -0300

    test folder stuff

---
+++ demo-api-restful/src/test/java/com/example/demoapirestful/DemoapirestfulApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.demoapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapirestfulApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
