50a7f8163d1f58de5ea345e1066834c7f084d759
Franco <francocorniglione5@gmail.com>
2024-11-15 12:53:15 -0300

    test folder stuff

---
+++ homework-design-patterns/src/test/java/com/example/homeworkdesignpatterns/HomeworkdesignpatternsApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.homeworkdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkdesignpatternsApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
