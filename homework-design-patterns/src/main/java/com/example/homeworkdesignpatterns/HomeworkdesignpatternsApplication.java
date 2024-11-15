48935001057c63688b4d9d6477a685da452f14a4
Franco <francocorniglione5@gmail.com>
2024-11-15 12:55:22 -0300

    entry point file

---
+++ homework-design-patterns/src/main/java/com/example/homeworkdesignpatterns/HomeworkdesignpatternsApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkdesignpatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkdesignpatternsApplication.class, args);
+	}
+
+}
