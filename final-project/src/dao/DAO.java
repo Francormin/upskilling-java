632a5f1c0b848f27117b545d736057c1cead850c
Franco <francocorniglione5@gmail.com>
2024-11-19 21:20:16 -0300

    crud common operations interface

---
+++ final-project/src/dao/DAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+    List<T> getAll();
+
+    T getById(int id);
+
+    void add(T t);
+
+    void update(int id, T t);
+
+    void delete(int id);
+}
