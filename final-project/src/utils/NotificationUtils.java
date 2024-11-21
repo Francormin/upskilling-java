13afc1aa297ef0058e414c280818bf79ed537581
Franco <francocorniglione5@gmail.com>
2024-11-21 11:53:29 -0300

    refactoring notification messages

--- final-project/src/utils/NotificationUtils.java
+++ final-project/src/utils/NotificationUtils.java
@@ -1,15 +1,24 @@
 package utils;
 
 public class NotificationUtils {
-    public static void showError(String message) {
-        /*
-        for (int i = 0; i < 256; i++) {
-            System.out.printf("\033[38;5;%dmColor %d  \033[0m", i, i);
-            if ((i + 1) % 8 == 0) {
-                System.out.println();
-            }
+    /*for (int i = 0; i < 256; i++) {
+        System.out.printf("\033[38;5;%dmColor %d  \033[0m", i, i);
+        if ((i + 1) % 8 == 0) {
+            System.out.println();
         }
-        */
+    }*/
+
+    public static void showOnError(String message) {
         System.out.println("\033[1m\033[38;5;173m\n" + message + "\033[0m");
+    }
+
+    public static void showOnSuccess(String message) {
+//        for (int i = 0; i < 256; i++) {
+//            System.out.printf("\033[38;5;%dmColor %d  \033[0m", i, i);
+//            if ((i + 1) % 8 == 0) {
+//                System.out.println();
+//            }
+//        }
+        System.out.println("\033[1m\033[38;5;34m\n" + message + "\033[0m");
     }
 }
