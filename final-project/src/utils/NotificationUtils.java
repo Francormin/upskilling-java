253dce2209f7f4903b7eeb1417b7ac0a937ee155
Franco <francocorniglione5@gmail.com>
2024-11-21 17:01:27 -0300

    code comment stuff

--- final-project/src/utils/NotificationUtils.java
+++ final-project/src/utils/NotificationUtils.java
@@ -13,12 +13,6 @@ public class NotificationUtils {
     }
 
     public static void showOnSuccess(String message) {
-//        for (int i = 0; i < 256; i++) {
-//            System.out.printf("\033[38;5;%dmColor %d  \033[0m", i, i);
-//            if ((i + 1) % 8 == 0) {
-//                System.out.println();
-//            }
-//        }
         System.out.println("\033[1m\033[38;5;34m\n" + message + "\033[0m");
     }
 }
