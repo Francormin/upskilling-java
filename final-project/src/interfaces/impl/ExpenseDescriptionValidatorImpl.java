e8f39b84a5da0fe1b0047cf3437662eb64dc4fa9
Franco <francocorniglione5@gmail.com>
2024-11-21 11:56:34 -0300

    message changed

--- final-project/src/interfaces/impl/ExpenseDescriptionValidatorImpl.java
+++ final-project/src/interfaces/impl/ExpenseDescriptionValidatorImpl.java
@@ -9,7 +9,7 @@ public class ExpenseDescriptionValidatorImpl implements ExpenseDescriptionValida
         if (!description.matches("^[a-zA-Z\\s]*$")) {
             throw new InvalidExpenseDescriptionException("La descripción solo puede contener letras.");
         } else if (description.length() < 5) {
-            throw new InvalidExpenseDescriptionException("La descripción debe tener un mínimo de 5 caracteres.");
+            throw new InvalidExpenseDescriptionException("La descripción debe tener al menos 5 letras.");
         }
     }
 }
