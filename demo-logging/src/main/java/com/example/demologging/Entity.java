f55a13fb3a4fca9a940e1b68adc12ee71415f133
Franco <francocorniglione5@gmail.com>
2024-11-22 10:11:45 -0300

    lombock first approach

---
+++ demo-logging/src/main/java/com/example/demologging/Entity.java
@@ -0,0 +1,23 @@
+package com.example.demologging;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@RequiredArgsConstructor
+@Data
+public class Entity {
+
+    private String name;
+    private Double price;
+
+}
