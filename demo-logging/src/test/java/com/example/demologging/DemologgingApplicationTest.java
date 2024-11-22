63c104a4efbed64f538b503780ca8ad472d82213
Franco <francocorniglione5@gmail.com>
2024-11-22 10:07:47 -0300

    test folder stuff

---
+++ demo-logging/src/test/java/com/example/demologging/DemologgingApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.demologging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemologgingApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
