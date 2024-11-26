9122c8791dd67e8f83c8d6b9dabf5439e7945d0a
Franco <francocorniglione5@gmail.com>
2024-11-26 15:42:03 -0300

    test folder stuff

---
+++ demo-orm/src/test/java/com/example/demoorm/DemoormApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.demoorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoormApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
