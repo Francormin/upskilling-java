8204622575672f716361c762a12202fb1ba0db16
Franco <francocorniglione5@gmail.com>
2024-11-26 16:12:01 -0300

    entry point file

---
+++ demo-orm/src/main/java/com/example/demoorm/DemoormApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoormApplication.class, args);
+	}
+
+}
