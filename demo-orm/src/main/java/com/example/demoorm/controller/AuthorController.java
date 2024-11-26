f7baada52de4e41b8f3b176c1c160a2770d1da26
Franco <francocorniglione5@gmail.com>
2024-11-26 15:45:14 -0300

    controller folder stuff

---
+++ demo-orm/src/main/java/com/example/demoorm/controller/AuthorController.java
@@ -0,0 +1,34 @@
+package com.example.demoorm.controller;
+
+import com.example.demoorm.entity.Author;
+import com.example.demoorm.service.AuthorService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public Author createAuthor(@RequestBody Author author) {
+        return authorService.createAuthor(author);
+    }
+
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+}
