f37be4e126d62ae202f87a09a4d919475e528a26
Franco <francocorniglione5@gmail.com>
2024-11-28 18:37:06 -0300

    exception folder stuff

---
+++ homework-orm/src/main/java/com/example/homeworkorm/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.homeworkorm.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Maneja excepciones de tipo EntityNotFoundException.
+     *
+     * @param ex Excepción lanzada.
+     * @return Respuesta HTTP con el mensaje de error.
+     */
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+}
