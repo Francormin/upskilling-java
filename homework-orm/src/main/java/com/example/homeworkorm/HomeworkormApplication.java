83e01a4c6e726d0fa0e8e08e0e9b35d2ce08183e
Franco <francocorniglione5@gmail.com>
2024-11-28 18:38:30 -0300

    entry point file

---
+++ homework-orm/src/main/java/com/example/homeworkorm/HomeworkormApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkormApplication.class, args);
+	}
+
+}
