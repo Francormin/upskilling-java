dee7de08e7e9c1848b5e0593589627f5c3e36671
Franco <francocorniglione5@gmail.com>
2024-11-28 18:34:56 -0300

    test folder stuff

---
+++ homework-orm/src/test/java/com/example/homeworkorm/HomeworkormApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.homeworkorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkormApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
