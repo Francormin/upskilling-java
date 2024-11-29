04e565afd45c1652fd952a0dec2f18a16937576a
Franco <francocorniglione5@gmail.com>
2024-11-29 19:21:36 -0300

    test folder stuff

---
+++ final-spring-project/src/test/java/com/javaupskilling/finalspringproject/ExpenseTrackerAppTest.java
@@ -0,0 +1,13 @@
+package com.javaupskilling.finalspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
