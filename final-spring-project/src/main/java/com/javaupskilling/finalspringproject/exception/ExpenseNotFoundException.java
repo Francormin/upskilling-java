029c52293244d22dfe706eadf4c5b31a88750e32
Franco <francocorniglione5@gmail.com>
2024-11-29 19:25:52 -0300

    exception folder stuff

---
+++ final-spring-project/src/main/java/com/javaupskilling/finalspringproject/exception/ExpenseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.javaupskilling.finalspringproject.exception;
+
+public class ExpenseNotFoundException extends RuntimeException {
+    public ExpenseNotFoundException(String message) {
+        super(message);
+    }
+}
