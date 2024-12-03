0aee29cfabc731db8d524bb84177efe692b5b61c
Franco <francocorniglione5@gmail.com>
2024-12-03 16:05:37 -0300

    entry point file

---
+++ homework-spring-data-jpa/src/main/java/com/example/homeworkspringdatajpa/HomeworkspringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkspringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkspringdatajpaApplication.class, args);
+	}
+
+}
