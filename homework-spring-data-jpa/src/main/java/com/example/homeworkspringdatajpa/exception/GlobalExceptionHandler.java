df1ca029c284ce006253bbeedbee1c0365837097
Franco <francocorniglione5@gmail.com>
2024-12-04 18:03:40 -0300

    small change

--- homework-spring-data-jpa/src/main/java/com/example/homeworkspringdatajpa/exception/GlobalExceptionHandler.java
+++ homework-spring-data-jpa/src/main/java/com/example/homeworkspringdatajpa/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<String> handleBookNotFoundException(BookNotFoundException ex) {
