69f748371473ee3e149b24d3254f307b4e6d1d18
Franco <francocorniglione5@gmail.com>
2024-12-04 12:33:06 -0300

    entry point file

---
+++ demo-test-integracion/src/main/java/com/example/demotestintegracion/DemotestintegracionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotestintegracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotestintegracionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotestintegracionApplication.class, args);
+    }
+
+}
