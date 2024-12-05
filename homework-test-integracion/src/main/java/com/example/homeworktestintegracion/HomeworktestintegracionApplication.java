da69583cef226a3c071b80443abb1f9ad487442b
Franco <francocorniglione5@gmail.com>
2024-12-05 10:15:42 -0300

    entry point file

---
+++ homework-test-integracion/src/main/java/com/example/homeworktestintegracion/HomeworktestintegracionApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworktestintegracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworktestintegracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworktestintegracionApplication.class, args);
+	}
+
+}
