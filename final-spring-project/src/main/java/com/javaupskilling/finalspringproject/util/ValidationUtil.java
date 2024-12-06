cfc2a6077cfaee1a883cfedc59b4e8c5b44c4442
Franco <francocorniglione5@gmail.com>
2024-12-06 17:10:18 -0300

    updating validation stuff

--- final-spring-project/src/main/java/com/javaupskilling/finalspringproject/util/ValidationUtil.java
+++ final-spring-project/src/main/java/com/javaupskilling/finalspringproject/util/ValidationUtil.java
@@ -6,14 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidationUtil {
 
     public static void validateId(Long id) {
-        log.info("Input received from params: {}", id);
+        log.info("validateId - Input received from params: {}", id);
         if (id <= 0) {
             throw new IllegalArgumentException("ID must be a positive number");
         }
     }
 
     public static void validateDate(String date) {
-        log.info("Input received from params: {}", date);
+        log.info("validateDate - Input received from params: {}", date);
         if (date.isBlank()) {
             throw new IllegalArgumentException("Date must not be blank");
         }
