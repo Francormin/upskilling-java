67df7f19db9670f8f9dbd6744643d800ecb87291
Franco <francocorniglione5@gmail.com>
2024-12-09 18:10:30 -0300

    improving DTOs with regexp and messages stuff

--- final-spring-project/src/main/java/com/javaupskilling/finalspringproject/dto/ExpenseRequestDto.java
+++ final-spring-project/src/main/java/com/javaupskilling/finalspringproject/dto/ExpenseRequestDto.java
@@ -3,6 +3,7 @@ package com.javaupskilling.finalspringproject.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -14,16 +15,23 @@ public class ExpenseRequestDto {
     private Double amount;
 
     @NotBlank(message = "Date cannot be null nor blank")
+    @Pattern(regexp = "^[0-9 -]+$", message = "Date can only contain numbers spaces, and hyphens")
     @Size(min = 10, max = 10, message = "Date must have 10 characters format: dd-MM-yyyy")
     private String date;
 
+    @Pattern(
+        regexp = "^[a-zA-Z.,:; ]+$",
+        message = "Description can only contain letters, spaces, and the following punctuation marks: . , : ;"
+    )
     @Size(min = 5, message = "Description must have a minimum of 5 characters")
     private String description;
 
     @NotNull(message = "ExpenseCategoryId cannot be null")
+    @Min(value = 1, message = "ExpenseCategoryId must be greater than zero")
     private Long expenseCategoryId;
 
     @NotNull(message = "UserId cannot be null")
+    @Min(value = 1, message = "UserId must be greater than zero")
     private Long userId;
 
 }
--- final-spring-project/src/main/java/com/javaupskilling/finalspringproject/dto/UserRequestDto.java
+++ final-spring-project/src/main/java/com/javaupskilling/finalspringproject/dto/UserRequestDto.java
@@ -4,6 +4,8 @@ import com.javaupskilling.finalspringproject.model.Expense;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -13,12 +15,23 @@ import java.util.List;
 public class UserRequestDto {
 
     @NotBlank(message = "Name cannot be null nor blank")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name can only contain letters")
+    @Size(min = 2, message = "Name must have a minimum of 2 letters")
     private String name;
 
     @NotBlank(message = "Surname cannot be null nor blank")
+    @Pattern(
+        regexp = "^[\\p{L}\\s'-]+$",
+        message = "Surname can only contain letters, spaces, hyphens, and apostrophes"
+    )
+    @Size(min = 2, message = "Surname must have a minimum of 2 letters")
     private String surname;
 
     @Email(message = "Email must be a valid one")
+    @Pattern(
+        regexp = "^[\\w-.]+@(\\w{3,}.)+\\w{2,4}$",
+        message = "Email must have this format: test@example.com"
+    )
     private String email;
 
     private List<Expense> expenses = new ArrayList<>();
