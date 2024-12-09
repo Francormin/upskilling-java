6f8d8881c866ccaf8f4c53d6cd5ca1e0c1379ce8
Franco <francocorniglione5@gmail.com>
2024-12-09 17:53:48 -0300

    altering schemas description column to TEXT

--- final-spring-project/src/main/resources/schema.sql
+++ final-spring-project/src/main/resources/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS expense_categories (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description VARCHAR(255)
+    description TEXT
 );
 
 -- Crear tabla de gastos
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS expenses (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     amount DOUBLE PRECISION NOT NULL,
     date VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
+    description TEXT,
     category_id BIGINT,
     user_id BIGINT NOT NULL,
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES expense_categories(id),
