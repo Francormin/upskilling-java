94b44ed98368f13bfdaffcd7c9354082105b43a7
Franco <francocorniglione5@gmail.com>
2024-12-10 09:50:36 -0300

    adding ExpenseCategory repository

--- final-spring-project/src/main/java/com/javaupskilling/finalspringproject/repository/ExpenseCategoryRepository.java
+++ final-spring-project/src/main/java/com/javaupskilling/finalspringproject/repository/ExpenseCategoryRepository.java
@@ -5,6 +5,11 @@ import com.javaupskilling.finalspringproject.model.ExpenseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExpenseCategoryRepository extends JpaRepository<ExpenseCategory, Long> {
+
+    List<ExpenseCategory> findByNameContainingIgnoreCase(String name);
+
 }
