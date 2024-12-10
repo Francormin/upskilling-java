f4efccfc0d5bb98f61e86bda6e528833e9a05c2a
Franco <francocorniglione5@gmail.com>
2024-12-10 09:50:04 -0300

    adding ExpenseCategory DTOs

---
+++ final-spring-project/src/main/java/com/javaupskilling/finalspringproject/dto/ExpenseCategoryRequestDto.java
@@ -0,0 +1,30 @@
+package com.javaupskilling.finalspringproject.dto;
+
+import com.javaupskilling.finalspringproject.model.Expense;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class ExpenseCategoryRequestDto {
+
+    @NotBlank(message = "Name cannot be null nor blank")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name can only contain letters")
+    @Size(min = 3, message = "Name must have a minimum of 3 letters")
+    private String name;
+
+    @Pattern(
+        regexp = "^[a-zA-Z.,:; ]+$",
+        message = "Description can only contain letters, spaces, and the following punctuation marks: . , : ;"
+    )
+    @Size(min = 5, message = "Description must have a minimum of 5 characters")
+    private String description;
+
+    private final List<Expense> expenses = new ArrayList<>();
+
+}
