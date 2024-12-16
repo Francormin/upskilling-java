768209cc507a2714e1f1f55a53b0bef745ecd813
Franco <francocorniglione5@gmail.com>
2024-12-16 18:10:56 -0300

    refactoring entity using lombok data annotation

--- homework-spring-data-jpa/src/main/java/com/example/homeworkspringdatajpa/entity/Book.java
+++ homework-spring-data-jpa/src/main/java/com/example/homeworkspringdatajpa/entity/Book.java
@@ -6,7 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "books")
 public class Book {
@@ -26,67 +28,5 @@ public class Book {
 
     @Column(name = "page_quantity")
     private Integer pageQuantity;
-
-    public Book() {
-    }
-
-    public Book(Long id, String title, String author, Integer publishYear, Integer pageQuantity) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.publishYear = publishYear;
-        this.pageQuantity = pageQuantity;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Integer getPublishYear() {
-        return publishYear;
-    }
-
-    public void setPublishYear(Integer publishYear) {
-        this.publishYear = publishYear;
-    }
-
-    public Integer getPageQuantity() {
-        return pageQuantity;
-    }
-
-    public void setPageQuantity(Integer pageQuantity) {
-        this.pageQuantity = pageQuantity;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-            "id=" + id +
-            ", title='" + title + '\'' +
-            ", author='" + author + '\'' +
-            ", publishYear=" + publishYear +
-            ", pageQuantity=" + pageQuantity +
-            '}';
-    }
 
 }
