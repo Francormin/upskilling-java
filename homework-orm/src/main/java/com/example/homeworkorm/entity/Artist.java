35ccc2c559cf6cbe8483d0470dab7d5930641edb
Franco <francocorniglione5@gmail.com>
2024-12-17 19:01:44 -0300

    artist entity updated

--- homework-orm/src/main/java/com/example/homeworkorm/entity/Artist.java
+++ homework-orm/src/main/java/com/example/homeworkorm/entity/Artist.java
@@ -1,7 +1,9 @@
 package com.example.homeworkorm.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,7 +30,7 @@ public class Artist {
 
     private String bio;
 
-    @OneToMany(mappedBy = "artist")
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Masterpiece> masterpieces = new ArrayList<>();
 
 }
